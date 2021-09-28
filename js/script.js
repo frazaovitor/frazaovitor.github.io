21c41bf9ee317b69e023fd1c94f67c913dd80168
frazaovitor <51264703+frazaovitor@users.noreply.github.com>
2021-09-27 21:20:39 -0300

    bug de clicar em qualquer parte sumir tudo(concertado)

--- js/script.js
+++ js/script.js
@@ -1,36 +1,74 @@
 let displayNone = document.querySelectorAll(".displayNone")
+let secaoPrincipal = document.querySelectorAll(".secaoPrincipal")
 
-document.querySelector('main').addEventListener("click", event => {
-    let ID = event.target.id
+secaoPrincipal.forEach(evento => {
+    evento.addEventListener("click", event => {
+        let ID = event.target.id
 
-    containerPrincipal.style = "display: none"
+        containerPrincipal.style = "display: none"
 
-    switch (ID) {
-        case "quemSou":
-            descricao.classList.remove("displayNone")
-            break;
+        switch (ID) {
+            case "quemSou":
+                descricao.classList.remove("displayNone")
+                break;
 
-        case "formacao":
-            formAcadem.classList.remove("displayNone")
-            break;
+            case "formacao":
+                formAcadem.classList.remove("displayNone")
+                break;
 
-        case "expProfissional":
-            expProf.classList.remove("displayNone")
-            break;
+            case "expProfissional":
+                expProf.classList.remove("displayNone")
+                break;
 
-        case "habilidades":
-            habProf.classList.remove("displayNone")
-            break;
+            case "habilidades":
+                habProf.classList.remove("displayNone")
+                break;
 
-        case "estudando":
-            estudAtual.classList.remove("displayNone")
-            break;
+            case "estudando":
+                estudAtual.classList.remove("displayNone")
+                break;
 
-        case "procurando":
-            procPor.classList.remove("displayNone")
-            break;
-    }
+            case "procurando":
+                procPor.classList.remove("displayNone")
+                break;
+        }
+    })
 })
+
+
+
+
+// document.querySelectorAll(".secaoPrincipal").addEventListener("click", event => {
+//     let ID = event.target.id
+
+//     containerPrincipal.style = "display: none"
+
+//     switch (ID) {
+//         case "quemSou":
+//             descricao.classList.remove("displayNone")
+//             break;
+
+//         case "formacao":
+//             formAcadem.classList.remove("displayNone")
+//             break;
+
+//         case "expProfissional":
+//             expProf.classList.remove("displayNone")
+//             break;
+
+//         case "habilidades":
+//             habProf.classList.remove("displayNone")
+//             break;
+
+//         case "estudando":
+//             estudAtual.classList.remove("displayNone")
+//             break;
+
+//         case "procurando":
+//             procPor.classList.remove("displayNone")
+//             break;
+//     }
+// })
 
 document.querySelector("#voltar").addEventListener("click", () => {
     displayNone.forEach(elemento => {
