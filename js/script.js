7675a668511f19485a3ec2debb1e3d4fe92e056f
frazaovitor <51264703+frazaovitor@users.noreply.github.com>
2021-09-30 20:14:13 -0300

    mudanças de ícones e fazendo "quem sou"

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let displayNone = document.querySelectorAll(".displayNone")
 let secaoPrincipal = document.querySelectorAll(".secaoPrincipal")
 
+
+// CLICANDO NOS BOTÕES
+
 secaoPrincipal.forEach(evento => {
     evento.addEventListener("click", event => {
         let ID = event.target.id
@@ -35,40 +38,7 @@ secaoPrincipal.forEach(evento => {
     })
 })
 
-
-
-
-// document.querySelectorAll(".secaoPrincipal").addEventListener("click", event => {
-//     let ID = event.target.id
-
-//     containerPrincipal.style = "display: none"
-
-//     switch (ID) {
-//         case "quemSou":
-//             descricao.classList.remove("displayNone")
-//             break;
-
-//         case "formacao":
-//             formAcadem.classList.remove("displayNone")
-//             break;
-
-//         case "expProfissional":
-//             expProf.classList.remove("displayNone")
-//             break;
-
-//         case "habilidades":
-//             habProf.classList.remove("displayNone")
-//             break;
-
-//         case "estudando":
-//             estudAtual.classList.remove("displayNone")
-//             break;
-
-//         case "procurando":
-//             procPor.classList.remove("displayNone")
-//             break;
-//     }
-// })
+// VOLTANDO AO TOPO OU PÁGINA PRINCIPAL
 
 document.querySelector("#voltar").addEventListener("click", () => {
     displayNone.forEach(elemento => {
